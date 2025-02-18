33f1a58bc519b9855d527af78ac05d25e32f86e2
FirdanAja <159457668+FirdanAja@users.noreply.github.com>
2025-02-18 23:14:17 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.{html,js}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
